e22bf7fbdd4e015b9d58b449e9a64567c57e8776
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2024-09-11 10:31:00 -0300

    Criando fluxo da aplicação

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Olá, mundo!");
+const { select } = require('@inquirer/prompts'); // Criar prompts interativos
+
+const start = async () => {
+  while (true) {
+    const opcao = await select({
+      message: "Menu >",
+      choices: [
+        {
+          name: "Cadastrar meta",
+          value: "cadastrar"
+        },
+        {
+          name: "Sair",
+          value: "sair"
+        }
+      ]
+    });
+  }
+}
+
+start();
